ab32d8d3bc58db701a1f4a407f59cb461b583461
MatusKocian <matus.kociann@gmail.com>
2024-06-01 00:03:57 +0200

    scoping data to user id

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,7 +3,14 @@ import { ConvexError, v } from "convex/values";
 
 export const getDocuments = query({
   async handler(ctx) {
-    return await ctx.db.query("documents").collect();
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+    if (!userId) {
+      return [];
+    }
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId))
+      .collect();
   },
 });
 
@@ -13,10 +20,12 @@ export const createDocument = mutation({
   },
   async handler(ctx, args) {
     const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
-    console.log("userId", userId);
     if (!userId) {
       throw new ConvexError("Not authenticated");
     }
-    await ctx.db.insert("documents", { title: args.title });
+    await ctx.db.insert("documents", {
+      title: args.title,
+      tokenIdentifier: userId,
+    });
   },
 });
